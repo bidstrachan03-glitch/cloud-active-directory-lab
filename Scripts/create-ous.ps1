3656437f7971bb9e3e71507a19bf05a39ecd27d0
bidstrachan03-glitch <bidstrachan03@gmail.com>
2026-06-16 22:56:21 -0400

    Create Organizational Units (OUs)

--- Scripts/create-ous.ps1
+++ Scripts/create-ous.ps1
@@ -1,1 +1,6 @@
+Automates the creation of Organizational Units to organize users based on departments.
 
+**Functions:**
+- Creates the Human Resources OU
+- Creates the IT Department OU
+- Verifies successful OU creation within Active Directory
